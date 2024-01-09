26245cd5c8935c181d3d08c4e21edd353731af81
AppDevSYS <151794430+AtmosferaSoltec@users.noreply.github.com>
2024-01-09 17:25:09 -0500

    priuebas

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { router } from './routes';
-import path from 'path';
 
 const puerto = process.env.PORT || 4000;
 const app = express();
@@ -10,16 +9,9 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/ping', (req, res) => {
-    res.send('pong local');
+    res.json('pong local');
 });
 
-app.use(express.static(path.join(__dirname, 'public'), {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-            res.type('application/javascript');
-        }
-    }
-}));
 
 app.use('/api', router);
 
